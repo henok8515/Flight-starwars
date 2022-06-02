38dd3420f25916174eda8b8b99dbb2b9f9567347
henok8515 <henokegezew44@gmail.com>
2022-06-02 22:59:30 +0300

    chore: cleaning up

--- src/components/PeopleList.js
+++ src/components/PeopleList.js
@@ -1,13 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 function PeopleList() {
-  useEffect(() => {
-    fetch("https://swapi.dev/api/people/")
-      .then((res) => res.json())
-      .then((resp) => {
-        setPeople(resp.results);
-      });
-  }, []);
   return <div>PeopleList</div>;
 }
 
--- src/components/PlanetList.js
+++ src/components/PlanetList.js
@@ -1,49 +1,7 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 
-const Planetlist = ({ setPlanet }) => {
-  const [planets, setPlanets] = useState([]);
-  const imgURL = "https://starwars-visualguide.com/assets/img/planets/";
+function PlanetList() {
+  return <div>PlanetList</div>;
+}
 
-  useEffect(() => {
-    fetch("https://swapi.dev/api/planets/")
-      .then((res) => res.json())
-      .then((resp) => {
-        setPlanets(resp.results);
-      });
-  }, []);
-
-  function getId(url) {
-    return url.split("/")[url.split("/").length - 2];
-  }
-
-  return (
-    <ul className="grid grid--columns grid--gap">
-      {planets.map((planet) => (
-        <li
-          key={planet.name}
-          style={{ backgroundImage: `url(${imgURL + getId(planet.url)}.jpg)` }}
-        >
-          <div className="desc">
-            <h3>{planet.name}</h3>
-            <p>{`Diameter: ${planet.diameter}`}</p>
-            <p>{planet.terrain}</p>
-            <a
-              href={`#planets/${planet.name}`}
-              onClick={() => setPlanet(getId(planet.url))}
-              role="button"
-            >
-              More details &raquo;
-            </a>
-          </div>
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-Planetlist.propTypes = {
-  setPlanet: PropTypes.func.isRequired,
-};
-
-export default Planetlist;
+export default PlanetList;
